27397f10b8b0cf7e07cee0c536d8d9240bf19804
binss-AUG <ignore6172@gmail.com>
2025-12-03 19:14:54 +0700

    feat: Thêm model của nhân viên, sản phẩm và nguyên liệu

---
+++ QuanLyCaPhe/Models/Ingredients.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCaPhe.Models
+{
+    class Ingredients
+    {
+        public int Id { get; set; }
+        public string IngName { get; set; }
+        public string Unit { get; set; }
+        public float Quantity { get; set; }
+    }
+}
---
+++ QuanLyCaPhe/Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyCaPhe.Models
+{
+    class Users
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public int Gender { get; set; }
+        public string PasswordHash { get; set; }
+        public string PasswordSalt { get; set; }
+        public string CreateAt { get; set; }
+        public string RoleName { get; set; }
+        public bool RoleLevel { get; set; }
+        public bool IsActive { get; set; }
+
+
+    }
+}
